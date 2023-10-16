bef1e529844d39e643cb381fbd13508e637d25eb
Vic <cruz.gomez.victor@gmail.com>
2023-10-16 11:54:05 -0400

    Add sidenav and toolbar

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styleUrls: ['./layout-page.component.css']
+  styleUrls: ['./layout-page.component.css'],
 })
 export class LayoutPageComponent {
-
+  public sidebarItems = [
+    { label: 'Listado', icon: 'label', url: './list' },
+    { label: 'Anadir', icon: 'add', url: './new-hero' },
+    { label: 'Buscar', icon: 'search', url: './search' },
+  ];
 }
