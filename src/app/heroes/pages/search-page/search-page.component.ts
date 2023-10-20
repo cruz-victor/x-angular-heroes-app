b0e7e45186cadbd398cf8f9c7b4568dff9bf64eb
Vic <cruz.gomez.victor@gmail.com>
2023-10-20 12:56:27 -0400

    Add material autocomplete

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/hereos.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -8,4 +11,32 @@ import { FormControl } from '@angular/forms';
 })
 export class SearchPageComponent {
   public searchInput=new FormControl();
+  public heroes:Hero[]=[];
+  public selectedHero?:Hero;
+
+  constructor(private heroesService:HeroesService){
+
+  }
+
+  searchHero(){
+    const value:string=this.searchInput.value||'';
+    console.log(value);
+
+    this.heroesService.getSuggestions(value)
+    .subscribe(heroes=>this.heroes = heroes);
+  }
+
+  onSelectedOption(event:MatAutocompleteSelectedEvent){
+    console.log(event.option.value);
+    if (!event.option.value) {
+      this.selectedHero=undefined;
+      return;
+    }
+
+    const hero:Hero=event.option.value;
+
+    this.searchInput.setValue(hero.superhero);
+
+    this.selectedHero = hero;
+  }
 }
