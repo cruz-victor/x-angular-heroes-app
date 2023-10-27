ec0cfed45d8912ae310f649e76f41d83d81dd1fa
Vic <cruz.gomez.victor@gmail.com>
2023-10-26 20:09:25 -0400

    Add edit and create options

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -4,6 +4,7 @@ import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-page',
@@ -14,12 +15,15 @@ export class NewPageComponent implements OnInit {
 
   constructor(private heroesService:HeroesService,
     private activatedRoute:ActivatedRoute,
-    private router:Router){
+    private router:Router,
+    private snackbar:MatSnackBar){
   }
 
   ngOnInit(): void {
+    //When is pressed Create option then It does nothing
     if(!this.router.url.includes('edit')) return;
 
+    //When is pressed Edit option next Retrieve Hero by its ID
     this.activatedRoute.params
     .pipe(
       switchMap(({id})=>this.heroesService.getHeroById(id))
@@ -36,6 +40,7 @@ export class NewPageComponent implements OnInit {
     throw new Error('Method not implemented.');
   }
 
+  //Form's fields
   public heroForm=new FormGroup({
     id: new FormControl<string>(''),
     superhero: new FormControl<string>('',{nonNullable:true}),
@@ -51,31 +56,42 @@ export class NewPageComponent implements OnInit {
     {id:'Marvel Comics', desc:'Marvel Comics'}
   ];
 
+  //Property Hero. It's get of from Form Hero
   get currentHero():Hero{
     const hero=this.heroForm.value as Hero;
 
     return hero;
   }
 
+  //Submit is option to send from data. This method works with Create option and Edit option
   onSubmit():void{
     if (!this.heroForm.valid) return;
 
+    //Update. It works when the option is update
     if(this.currentHero.id){
       this.heroesService.updateHero(this.currentHero)
       .subscribe(hero=>{
         //TODO: mostrar snackbar
         console.log('...update hero');
+        this.showSnackbar(`${hero.superhero} updated!`)
       });
 
       return;
     }
 
+    //Create. It works when the option is create
     this.heroesService.addHero(this.currentHero)
     .subscribe(hero=>{
       //TODO: mostrar snackbar
       console.log('...insert hero');
+      this.router.navigate(['/heroes/edit', hero.id]);
+      this.showSnackbar(`${hero.superhero} created!`);
     });
 
+  }
+
+  showSnackbar(message:string):void{
+    this.snackbar.open(message, 'done', {duration:2500});
   }
 
 }
